1481ba637104aa1095f5214624eb43dfdbba35f5
Gabriel Dias <dias.1618@gmail.com>
2020-11-09 13:02:59 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://www.dias1618.host/safelist/api/',
+  baseUrl: 'https://www.dias1618.host/safelist/api',
   axiosConfig: { headers: {
       common: {
           "Cache-Control": "no-cache, no-store, must-revalidate",
