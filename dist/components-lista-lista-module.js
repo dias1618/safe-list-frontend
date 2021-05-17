46db3f52e23d63b2999f0f0a6d887a75ef0aab23
Gabriel Dias <dias.1618@gmail.com>
2021-05-17 10:32:02 -0300

    fix/correcao-tamanho-fonte-lista

--- dist/components-lista-lista-module.js
+++ dist/components-lista-lista-module.js
@@ -12157,24 +12157,24 @@ let ListaParticipantesPdf = class ListaParticipantesPdf {
             this.generateReport.addTitle(this.lista.nomeCapela, { fontSize: 16 });
             this.generateReport.addSubtitle(`Lista de participantes do dia ${Object(date_fns__WEBPACK_IMPORTED_MODULE_3__["format"])(new Date(this.lista.data), 'dd/MM/yyyy')} (${Object(date_fns__WEBPACK_IMPORTED_MODULE_3__["format"])(new Date(this.lista.horaInicial), 'HH:mm')} - ${Object(date_fns__WEBPACK_IMPORTED_MODULE_3__["format"])(new Date(this.lista.horaFinal), 'HH:mm')})`, { fontSize: 14 });
             this.generateReport.addRow([
-                { text: `Posição`, fontSize: 12, margin: 20 },
-                { text: `Presença`, fontSize: 12, margin: 80 },
-                { text: `Participante`, fontSize: 12, margin: 140 },
-                { text: `Telefone`, fontSize: 12, margin: 480 },
+                { text: `Posição`, fontSize: 14, margin: 20 },
+                { text: `Presença`, fontSize: 14, margin: 80 },
+                { text: `Participante`, fontSize: 14, margin: 140 },
+                { text: `Telefone`, fontSize: 14, margin: 480 },
             ]);
             let quantParticipantes = 1;
             for (let participante of this.lista.participantes) {
                 this.generateReport.addRow([
-                    { text: `${((quantParticipantes++))}`, fontSize: 12, margin: 20 },
+                    { text: `${((quantParticipantes++))}`, fontSize: 14, margin: 20 },
                     { text: `${participante.nome}${participante.telefone}`, checkbox: true, margin: 80 },
-                    { text: `${participante.nome}`, fontSize: 12, margin: 140 },
-                    { text: `${this.maskTelefone(participante.telefone)}`, fontSize: 12, margin: 480 }
+                    { text: `${participante.nome}`, fontSize: 14, margin: 140 },
+                    { text: `${this.maskTelefone(participante.telefone)}`, fontSize: 14, margin: 480 }
                 ]);
                 for (let dependente of participante.dependentes) {
                     this.generateReport.addRow([
-                        { text: `${((quantParticipantes++))}`, fontSize: 12, margin: 20 },
+                        { text: `${((quantParticipantes++))}`, fontSize: 14, margin: 20 },
                         { text: `${participante.nome}${participante.telefone}${dependente.nome}`, checkbox: true, margin: 80 },
-                        { text: `${dependente.nome}`, fontSize: 12, margin: 180 }
+                        { text: `${dependente.nome}`, fontSize: 14, margin: 180 }
                     ]);
                 }
             }
