e6662be877108ad46b364a042dcf3d7e475a474a
Gabriel Dias <dias.1618@gmail.com>
2021-08-31 10:25:39 -0300

    atualizacao participantes

--- src/app/providers/configuracoes.provider.ts
+++ src/app/providers/configuracoes.provider.ts
@@ -2,7 +2,7 @@ export class Configuracoes{
     private _maxParticipantes:number;
 
     constructor(){
-        this._maxParticipantes = 380;
+        this._maxParticipantes = 400;
     }
 
     get maxParticipantes():number{
